a266a34855b5b743aceeb2dd893e8988fbbd5794
VolodyaLetov <tregvovka@gmail.com>
2025-08-06 12:06:50 +0200

    added textures

--- scripts/main.js
+++ scripts/main.js
@@ -24,6 +24,22 @@ scene.add(light);
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.update(); // initialize controls
 
+//texture
+
+//function creating texture
+function makeStickerTexture(colorHex) {
+  const canvas = document.createElement("canvas");
+  canvas.width = 64;
+  canvas.height = 64;
+  const context = canvas.getContext("2d");
+  context.fillStyle = colorHex;
+  context.fillRect(0, 0, 64, 64);
+  context.strokeStyle = "#000000";
+  context.lineWidth = 4;
+  context.strokeRect(0, 0, 64, 64);
+  const texture = new THREE.CanvasTexture(canvas);
+  return new THREE.CanvasTexture(canvas);
+}
 // Create a 3x3x3 grid of cubes (Rubik’s cube pieces)
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
 const cubelets = [];
@@ -32,23 +48,36 @@ for (let y = 1; y >= -1; y--) {
     for (let z = -1; z <= 1; z++) {
       const material = [];
       material.push(
-        new THREE.MeshBasicMaterial({ color: x === 1 ? 0xff0000 : 0x000000 })
+        new THREE.MeshBasicMaterial({
+          map: makeStickerTexture(x === 1 ? "#ff0000" : "#000000"),
+        })
       );
       material.push(
-        new THREE.MeshBasicMaterial({ color: x === -1 ? 0xffa500 : 0x000000 })
+        new THREE.MeshBasicMaterial({
+          map: makeStickerTexture(x === -1 ? "#ff7300ff" : "#000000"),
+        })
       );
       material.push(
-        new THREE.MeshBasicMaterial({ color: y === 1 ? 0xffff00 : 0x000000 })
+        new THREE.MeshBasicMaterial({
+          map: makeStickerTexture(y === 1 ? "#ffff00" : "#000000"),
+        })
       );
       material.push(
-        new THREE.MeshBasicMaterial({ color: y === -1 ? 0xffffff : 0x000000 })
+        new THREE.MeshBasicMaterial({
+          map: makeStickerTexture(y === -1 ? "#ffffff" : "#000000"),
+        })
       );
       material.push(
-        new THREE.MeshBasicMaterial({ color: z === 1 ? 0x0000ff : 0x000000 })
+        new THREE.MeshBasicMaterial({
+          map: makeStickerTexture(z === 1 ? "#0000ff" : "#000000"),
+        })
       );
       material.push(
-        new THREE.MeshBasicMaterial({ color: z === -1 ? 0x00ff00 : 0x000000 })
+        new THREE.MeshBasicMaterial({
+          map: makeStickerTexture(z === -1 ? "#00ff00" : "#000000"),
+        })
       );
+
       const cubelet = new THREE.Mesh(boxGeometry, material);
       cubelet.position.set(x, y, z);
       // Store original position in userData (optional)
